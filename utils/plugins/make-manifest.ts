6fbc8240de7185a63cfbb4547a1f7d3aeaae1ea8
Kusal Kithul-Godage <git@kusal.io>
2023-04-17 02:33:12 +1000

    Bugfix make-manifest.ts

--- utils/plugins/make-manifest.ts
+++ utils/plugins/make-manifest.ts
@@ -21,10 +21,12 @@ export default function makeManifest(
 
     // Naming change for cache invalidation
     if (config.contentScriptCssKey) {
-      manifest.content_scripts.forEach((script) => {
-        script.css = script.css.map((css) =>
-          css.replace("<KEY>", config.contentScriptCssKey)
-        );
+      manifest.content_scripts.forEach(script => {
+        if (script.css) {
+          script.css = script.css.map(css =>
+            css.replace("<KEY>", config.contentScriptCssKey)
+          );
+        }
       });
     }
 
